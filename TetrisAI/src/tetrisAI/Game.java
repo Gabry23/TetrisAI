3dc5fff64d3f9da0b5a6d19e42562dad78f8d077
Davide Cirimele <davideciri7@gmail.com>
2021-03-12 01:33:07 +0100

    id Celle, creazione nuovi pezzi

--- TetrisAI/src/tetrisAI/Game.java
+++ TetrisAI/src/tetrisAI/Game.java
@@ -1,5 +1,7 @@
 package tetrisAI;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import javax.swing.JFrame;
@@ -34,13 +36,21 @@ public class Game implements Runnable {
 	@Override
 	public void run() {
 		
-		Piece currPiece = createPiece();
+		List<Piece> pieces = new ArrayList<Piece>();
 		
-
+		boolean updatable;
+		
+		pieces.add(createPiece());
+		Piece currPiece = pieces.get(pieces.size()-1);
 	
 		while(true) {
-			
 		
+		if(!currPiece.isMoving()) {
+			pieces.add(createPiece());
+			currPiece = pieces.get(pieces.size()-1);
+		}
+			
+		updatable=true;
 			
 		
 			for(int i=0; i<map.getMatrix().length; i++) {
@@ -53,73 +63,40 @@ public class Game implements Runnable {
 
 						if( checkScrollCondition(map, currPiece)) {
 												
-							
-						/*	for(int k=0; k<4; k++) {
-						 
-								currPiece.getPiece()[k].setRow(currPiece.getPiece()[k].getRow()+1);
-								map.getSuppMatrix()[currPiece.getPiece()[k].getRow()][currPiece.getPiece()[k].getColumn()] = currPiece.getPiece()[k]; 
+							if(updatable) {	
+								for(int k=0; k<4; k++) {
+									currPiece.getPiece()[k].setRow(currPiece.getPiece()[k].getRow()+1);
+									map.getSuppMatrix()[currPiece.getPiece()[k].getRow()][currPiece.getPiece()[k].getColumn()] = currPiece.getPiece()[k]; 
+								}
+							updatable=false;
 							}
 								
-						*/
-							
-							
-							if(currPiece.getPiece()[0].getRow() == i && currPiece.getPiece()[0].getColumn() == j){
-								currPiece.getPiece()[0].setRow(currPiece.getPiece()[0].getRow()+1);
-				
-								map.getSuppMatrix()[currPiece.getPiece()[0].getRow()][currPiece.getPiece()[0].getColumn()] = currPiece.getPiece()[0]; 
-							}
-							else if(currPiece.getPiece()[1].getRow() == i && currPiece.getPiece()[1].getColumn() == j){
-								currPiece.getPiece()[1].setRow(currPiece.getPiece()[1].getRow()+1);
-					
-								map.getSuppMatrix()[currPiece.getPiece()[1].getRow()][currPiece.getPiece()[1].getColumn()] = currPiece.getPiece()[1];
-							}
-							
-							else if(currPiece.getPiece()[2].getRow() == i && currPiece.getPiece()[2].getColumn() == j){
-								currPiece.getPiece()[2].setRow(currPiece.getPiece()[2].getRow()+1);
-					
-								map.getSuppMatrix()[currPiece.getPiece()[2].getRow()][currPiece.getPiece()[2].getColumn()] = currPiece.getPiece()[2];
-							}
-
-							else if(currPiece.getPiece()[3].getRow() == i && currPiece.getPiece()[3].getColumn() == j){
-								currPiece.getPiece()[3].setRow(currPiece.getPiece()[3].getRow()+1);
-						
-								map.getSuppMatrix()[currPiece.getPiece()[3].getRow()][currPiece.getPiece()[3].getColumn()] = currPiece.getPiece()[3];
-							}
-							
-								
-				//			currPiece.getPiece()[z].setRow(currPiece.getPiece()[z].getRow()+1);
-					//		System.out.println(currPiece.getPiece()[z].getRow());
-						//	map.getSuppMatrix()[currPiece.getPiece()[z].getRow()][currPiece.getPiece()[z].getColumn()].setValue(currPiece.getPiece()[z].getValue()); 
-						
-						
-
-							//currPiece = createPiece();
 						}
 									
-					else {
-						System.out.println("a");
-						currPiece.setMoving(false);
-						for(int k=0; k<4; k++) {
-							currPiece.getPiece()[k].setValue(currPiece.getPiece()[k].getValue()*-1);
+						else {
+							System.out.println("a");
+							currPiece.setMoving(false);
+							for(int k=0; k<4; k++) {
+								currPiece.getPiece()[k].setValue(currPiece.getPiece()[k].getValue()*-1);
 						
-							System.out.println(currPiece.getPiece()[k].getRow());
-							System.out.println(currPiece.getPiece()[k].getColumn());
-						}
-
+								System.out.println(currPiece.getPiece()[k].getRow());
+								System.out.println(currPiece.getPiece()[k].getColumn());
+								System.out.println(currPiece.getPiece()[k].getValue());
+							}
 
 						//System.out.println(currPiece.getPiece()[0].getRow());
 					
 				
-					}
+						}
 		
-				}
+					}
 				
 				}
 				
 			
 			}
 			Copy(map.getMatrix(), map.getSuppMatrix());		
-			clearSuppMatrix(currPiece);
+			clearSuppMatrix();
 			
 			map.update();
 			try {
@@ -138,16 +115,15 @@ public class Game implements Runnable {
 	
 	
 	
-	private void clearSuppMatrix(Piece currPiece) {
+	private void clearSuppMatrix() {
 
 		for(int i=0; i<map.matrix.length; i++) {
 			for(int j=0; j<map.matrix[i].length; j++) {
 			
-				if(map.getSuppMatrix()[i][j].getValue()>=0) {
-						
-				eb.setRow(i);
-				eb.setColumn(j);
-				map.getSuppMatrix()[i][j] =  eb;
+				if(map.getSuppMatrix()[i][j].getValue()>=0) {	
+					eb.setRow(i);
+					eb.setColumn(j);
+					map.getSuppMatrix()[i][j] =  eb;
 				}
 				
 				
@@ -184,7 +160,7 @@ public class Game implements Runnable {
 			
 			if(currPiece.getPiece()[i].getRow()<=18) {
 				
-			if(map.getMatrix()[currPiece.getPiece()[i].getRow()+1][currPiece.getPiece()[i].getColumn()].getId() != map.getMatrix()[currPiece.getPiece()[i].getRow()][currPiece.getPiece()[i].getColumn()].getId()) {
+			if(map.getMatrix()[currPiece.getPiece()[i].getRow()+1][currPiece.getPiece()[i].getColumn()].getId()!=currPiece.getId() && map.getMatrix()[currPiece.getPiece()[i].getRow()+1][currPiece.getPiece()[i].getColumn()].getValue()!=0) {
 				return false;
 			}
 			}
@@ -205,7 +181,7 @@ public class Game implements Runnable {
 		
 		Random random = new Random();
 		int min = 1; // numero minimo
-		int max = 8; // numero massimo
+		int max = 7; // numero massimo
 		int c = ((max-min) + 1);
 		int rand = random.nextInt(c) + min;
 		Piece piece = null;
@@ -213,74 +189,93 @@ public class Game implements Runnable {
 		
 		if(rand==1) {
 			piece = new iBlock();
-			map.matrix[((iBlock) piece).getPiece()[0].getRow()][((iBlock) piece).getPiece()[0].getColumn()] = ((iBlock) piece).getPiece()[0];
-			map.matrix[((iBlock) piece).getPiece()[1].getRow()][((iBlock) piece).getPiece()[1].getColumn()] = ((iBlock) piece).getPiece()[1];
-			map.matrix[((iBlock) piece).getPiece()[2].getRow()][((iBlock) piece).getPiece()[2].getColumn()] = ((iBlock) piece).getPiece()[2];
-			map.matrix[((iBlock) piece).getPiece()[3].getRow()][((iBlock) piece).getPiece()[3].getColumn()] = ((iBlock) piece).getPiece()[3];
+			addPiece(piece);
 			((iBlock) piece).setMoving(true);
+			
+			piece.setId(id);
+			
+			for(int k=0;k<4;k++)
+			piece.getPiece()[k].setId(id);
+			
 		}
 		
 		if(rand==2) {
 			piece = new jBlock();
-			map.matrix[((jBlock) piece).getPiece()[0].getRow()][((jBlock) piece).getPiece()[0].getColumn()] = ((jBlock) piece).getPiece()[0];
-			map.matrix[((jBlock) piece).getPiece()[1].getRow()][((jBlock) piece).getPiece()[1].getColumn()] = ((jBlock) piece).getPiece()[1];
-			map.matrix[((jBlock) piece).getPiece()[2].getRow()][((jBlock) piece).getPiece()[2].getColumn()] = ((jBlock) piece).getPiece()[2];
-			map.matrix[((jBlock) piece).getPiece()[3].getRow()][((jBlock) piece).getPiece()[3].getColumn()] = ((jBlock) piece).getPiece()[3];
+			addPiece(piece);
 			((jBlock) piece).setMoving(true);
+			
+			piece.setId(id);
+			
+			for(int k=0;k<4;k++)
+			piece.getPiece()[k].setId(id);
 		}
 		
 		if(rand==3) {
 			piece = new lBlock();
-			map.matrix[((lBlock) piece).getPiece()[0].getRow()][((lBlock) piece).getPiece()[0].getColumn()] = ((lBlock) piece).getPiece()[0];
-			map.matrix[((lBlock) piece).getPiece()[1].getRow()][((lBlock) piece).getPiece()[1].getColumn()] = ((lBlock) piece).getPiece()[1];
-			map.matrix[((lBlock) piece).getPiece()[2].getRow()][((lBlock) piece).getPiece()[2].getColumn()] = ((lBlock) piece).getPiece()[2];
-			map.matrix[((lBlock) piece).getPiece()[3].getRow()][((lBlock) piece).getPiece()[3].getColumn()] = ((lBlock) piece).getPiece()[3];
+			addPiece(piece);
 			((lBlock) piece).setMoving(true);
+			
+			piece.setId(id);
+			
+			for(int k=0;k<4;k++)
+			piece.getPiece()[k].setId(id);
 		}
 		
 		
 		if(rand==4) {
 			piece = new oBlock();
-			map.matrix[((oBlock) piece).getPiece()[0].getRow()][((oBlock) piece).getPiece()[0].getColumn()] = ((oBlock) piece).getPiece()[0];
-			map.matrix[((oBlock) piece).getPiece()[1].getRow()][((oBlock) piece).getPiece()[1].getColumn()] = ((oBlock) piece).getPiece()[1];
-			map.matrix[((oBlock) piece).getPiece()[2].getRow()][((oBlock) piece).getPiece()[2].getColumn()] = ((oBlock) piece).getPiece()[2];
-			map.matrix[((oBlock) piece).getPiece()[3].getRow()][((oBlock) piece).getPiece()[3].getColumn()] = ((oBlock) piece).getPiece()[3];
+			addPiece(piece);
 			((oBlock) piece).setMoving(true);
+			
+			piece.setId(id);
+			
+			for(int k=0;k<4;k++)
+			piece.getPiece()[k].setId(id);
 		}
 		
 		
 		if(rand==5) {
 			piece = new sBlock();
-			map.matrix[((sBlock) piece).getPiece()[0].getRow()][((sBlock) piece).getPiece()[0].getColumn()] = ((sBlock) piece).getPiece()[0];
-			map.matrix[((sBlock) piece).getPiece()[1].getRow()][((sBlock) piece).getPiece()[1].getColumn()] = ((sBlock) piece).getPiece()[1];
-			map.matrix[((sBlock) piece).getPiece()[2].getRow()][((sBlock) piece).getPiece()[2].getColumn()] = ((sBlock) piece).getPiece()[2];
-			map.matrix[((sBlock) piece).getPiece()[3].getRow()][((sBlock) piece).getPiece()[3].getColumn()] = ((sBlock) piece).getPiece()[3];
+			addPiece(piece);
 			((sBlock) piece).setMoving(true);
+			
+			piece.setId(id);
+			
+			for(int k=0;k<4;k++)
+			piece.getPiece()[k].setId(id);
 		}
 		
 		
 		if(rand==6) {
 			piece = new tBlock();
-			map.matrix[((tBlock) piece).getPiece()[0].getRow()][((tBlock) piece).getPiece()[0].getColumn()] = ((tBlock) piece).getPiece()[0];
-			map.matrix[((tBlock) piece).getPiece()[1].getRow()][((tBlock) piece).getPiece()[1].getColumn()] = ((tBlock) piece).getPiece()[1];
-			map.matrix[((tBlock) piece).getPiece()[2].getRow()][((tBlock) piece).getPiece()[2].getColumn()] = ((tBlock) piece).getPiece()[2];
-			map.matrix[((tBlock) piece).getPiece()[3].getRow()][((tBlock) piece).getPiece()[3].getColumn()] = ((tBlock) piece).getPiece()[3];
+			addPiece(piece);
 			((tBlock) piece).setMoving(true);
+			
+			piece.setId(id);
+			
+			for(int k=0;k<4;k++)
+			piece.getPiece()[k].setId(id);
 		}
 		
 		if(rand==7) {
 			piece = new zBlock();
-			map.matrix[((zBlock) piece).getPiece()[0].getRow()][((zBlock) piece).getPiece()[0].getColumn()] = ((zBlock) piece).getPiece()[0];
-			map.matrix[((zBlock) piece).getPiece()[1].getRow()][((zBlock) piece).getPiece()[1].getColumn()] = ((zBlock) piece).getPiece()[1];
-			map.matrix[((zBlock) piece).getPiece()[2].getRow()][((zBlock) piece).getPiece()[2].getColumn()] = ((zBlock) piece).getPiece()[2];
-			map.matrix[((zBlock) piece).getPiece()[3].getRow()][((zBlock) piece).getPiece()[3].getColumn()] = ((zBlock) piece).getPiece()[3];
+			addPiece(piece);
 			((zBlock) piece).setMoving(true);
+			
+			piece.setId(id);
+			
+			for(int k=0;k<4;k++)
+			piece.getPiece()[k].setId(id);
 		}
-		piece.setId(id);
 		id++;
 		return piece;
 		
 		
+	}
+	
+	public void addPiece(Piece piece) {
+		for(int i=0;i<4;i++)
+		map.matrix[piece.getPiece()[i].getRow()][piece.getPiece()[i].getColumn()] = piece.getPiece()[i];
 	}
 	
 	
