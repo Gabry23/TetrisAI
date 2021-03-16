b7fceea610c779133a53af3c3c54137b5eced8cb
Davide Cirimele <davideciri7@gmail.com>
2021-03-16 18:15:02 +0100

    Asp Solver add

--- TetrisAI/src/tetrisAI/ASPSolver.java
+++ TetrisAI/src/tetrisAI/ASPSolver.java
@@ -2,47 +2,102 @@ package tetrisAI;
 
 import it.unical.mat.embasp.base.Handler;
 import it.unical.mat.embasp.base.InputProgram;
+import it.unical.mat.embasp.base.Output;
 import it.unical.mat.embasp.languages.IllegalAnnotationException;
 import it.unical.mat.embasp.languages.ObjectNotValidException;
 import it.unical.mat.embasp.languages.asp.ASPInputProgram;
 import it.unical.mat.embasp.languages.asp.ASPMapper;
+import it.unical.mat.embasp.languages.asp.AnswerSets;
 import it.unical.mat.embasp.platforms.desktop.DesktopHandler;
 import it.unical.mat.embasp.specializations.dlv2.desktop.DLV2DesktopService;
 
 public class ASPSolver {
-	private static String encodingResource="encodings/DotsAndBoxes";
+	
+	private static String encodingResource="encodings/tetris";
     private static Handler handler;
     private InputProgram facts;
-    
-	public ASPSolver() {
-		if(System.getProperty("os.name").equals("Mac OS X")) {
-            handler = new DesktopHandler(new DLV2DesktopService("lib/dlv2.mac_7"));
-            //handler.addOption(new OptionDescriptor("-n 0"));
-            System.out.println("Sistema operativo Mac OS X");
-        }
-        else if(System.getProperty("os.name").equals("Windows 10")) {
-            handler = new DesktopHandler(new DLV2DesktopService("lib/dlv2.exe"));
-            //handler.addOption(new OptionDescriptor("-n 0"));
-            System.out.println("Sistema operativo Windows 10");
-        }
-        else {
-            System.out.println("Errore, sistema operativo non riconosciuto con IDLV");
-        }
-                
-                
-        //classe Edge che viene prima registrata all'ASPMapper
-		/*try {
-            ASPMapper.getInstance().registerClass(Edge.class);
-            ASPMapper.getInstance().registerClass(Size.class);
-            ASPMapper.getInstance().registerClass(Assegno.class);
-        } catch (ObjectNotValidException | IllegalAnnotationException e1) {
-            e1.printStackTrace();
-        }*/
-        facts= new ASPInputProgram();
-        
-        InputProgram encoding= new ASPInputProgram();
-        encoding.addFilesPath(encodingResource);
-        handler.addProgram(encoding);
-	}
+    private InputProgram variableFacts;
 
+public ASPSolver() {
+	
+	if(System.getProperty("os.name").equals("Mac OS X")) {
+        handler = new DesktopHandler(new DLV2DesktopService("lib/dlv2.mac_7"));
+        //handler.addOption(new OptionDescriptor("-n 0"));
+        System.out.println("Sistema operativo Mac OS X");
+    }
+    else if(System.getProperty("os.name").equals("Windows 10")) {
+        handler = new DesktopHandler(new DLV2DesktopService("lib/dlv2.exe"));
+        //handler.addOption(new OptionDescriptor("-n 0"));
+        System.out.println("Sistema operativo Windows 10");
+    }
+    else {
+        System.out.println("Errore, sistema operativo non riconosciuto con IDLV");
+    }
+            
+            
+    //classe Edge che viene prima registrata all'ASPMapper
+    try {
+        ASPMapper.getInstance().registerClass(Cell.class);
+        ASPMapper.getInstance().registerClass(Piece.class);
+        ASPMapper.getInstance().registerClass(iBlock.class);
+      //  ASPMapper.getInstance().registerClass(Assegno.class);
+    } catch (ObjectNotValidException | IllegalAnnotationException e1) {
+        e1.printStackTrace();
+    }
+    facts= new ASPInputProgram();
+    
+    for(int i=0; i<20; i++) {
+    	for(int j=0; j<10; j++) {
+    try {
+		facts.addObjectInput(new Cell(i, j, 0));
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    }
+    }
+    handler.addProgram(facts);
+    
+    System.out.println(facts.getPrograms());
+    
+    InputProgram encoding= new ASPInputProgram();
+    encoding.addFilesPath(encodingResource);
+    handler.addProgram(encoding);
+    
 }
+
+
+public void addPiece(Piece piece) {
+	
+	int X1 = ((iBlock) piece).getX1();
+	int X2 = ((iBlock) piece).getX2();
+	int X3 = ((iBlock) piece).getX3();
+	int X4 = ((iBlock) piece).getX4();
+	
+	int Y1 = ((iBlock) piece).getY1();
+	int Y2 = ((iBlock) piece).getY2();
+	int Y3 = ((iBlock) piece).getY3();
+	int Y4 = ((iBlock) piece).getY4();
+	try {
+		variableFacts.addObjectInput(new iBlock(X1,Y1,X2,Y2,X3,Y3,X4,Y4,1));
+	    handler.addProgram(variableFacts);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    System.out.println(facts.getPrograms());
+}
+
+/*public Cell getNextMove() {
+	Output o = handler.startSync();
+	AnswerSets answersets = (AnswerSets) o;
+	
+	for(Object obj:)
+	return null;
+	
+	
+	
+}*/
+}
+
+
--- TetrisAI/src/tetrisAI/iBlock.java
+++ TetrisAI/src/tetrisAI/iBlock.java
@@ -8,11 +8,33 @@ import javax.imageio.ImageIO;
 import it.unical.mat.embasp.languages.Id;
 import it.unical.mat.embasp.languages.Param;
 		
-@Id("iBlock")
+@Id("iblock")
 public class iBlock extends Piece {
 
-	@Param(1)
+	
 	private int id = 1;
+	
+	@Param(1)
+	private int X1;
+	@Param(2)
+	private int Y1;
+	@Param(3)
+	private int X2;
+	@Param(4)
+	private int Y2;
+	@Param(5)
+	private int X3;
+	@Param(6)
+	private int Y3;
+	@Param(7)
+	private int X4;
+	@Param(8)
+	private int Y4;
+	@Param(9)
+	private int value = 1;
+	
+	
+	
 	
 	public iBlock() {
 		
@@ -31,6 +53,17 @@ public class iBlock extends Piece {
 		blocks[3].setValue(1);
 		this.setState(true, 0);
 		
+		X1 = blocks[0].getRow();
+		Y1 = blocks[0].getColumn();
+		
+		X2 = blocks[1].getRow();
+		Y2 = blocks[1].getColumn();
+		
+		X3 = blocks[2].getRow();
+		Y3 = blocks[2].getColumn();
+		
+		X4 = blocks[3].getRow();
+		Y4 = blocks[3].getColumn();
 		
 		try {
 			image = ImageIO.read(this.getClass().getResource("/resources/cyan.png"));
@@ -44,6 +77,89 @@ public class iBlock extends Piece {
 		
 	}
 	
+	public iBlock(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int i) {
+		X1 = x1;
+		X2 = x2;
+		X3 = x3;
+		X4 = x4;
+		Y1 = y1;
+		Y2 = y2;
+		Y3 = y3;
+		Y4 = y4;
+	}
+
+	public int getX1() {
+		return X1;
+	}
+
+	public void setX1(int x1) {
+		X1 = x1;
+	}
+
+	public int getY1() {
+		return Y1;
+	}
+
+	public void setY1(int y1) {
+		Y1 = y1;
+	}
+
+	public int getX2() {
+		return X2;
+	}
+
+	public void setX2(int x2) {
+		X2 = x2;
+	}
+
+	public int getY2() {
+		return Y2;
+	}
+
+	public void setY2(int y2) {
+		Y2 = y2;
+	}
+
+	public int getX3() {
+		return X3;
+	}
+
+	public void setX3(int x3) {
+		X3 = x3;
+	}
+
+	public int getY3() {
+		return Y3;
+	}
+
+	public void setY3(int y3) {
+		Y3 = y3;
+	}
+
+	public int getX4() {
+		return X4;
+	}
+
+	public void setX4(int x4) {
+		X4 = x4;
+	}
+
+	public int getY4() {
+		return Y4;
+	}
+
+	public void setY4(int y4) {
+		Y4 = y4;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
 	public iBlock getType() {
 		return this;
 	}
