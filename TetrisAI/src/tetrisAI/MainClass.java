0681dd8f2aa0f5e4300bb08b34690141fcdba2fd
davidecirimele <davideciri7@gmail.com>
2021-07-14 18:36:35 +0200

    Prova

--- TetrisAI/src/tetrisAI/MainClass.java
+++ TetrisAI/src/tetrisAI/MainClass.java
@@ -2,19 +2,6 @@ package tetrisAI;
 
 import javax.swing.JFrame;
 
-import it.unical.mat.embasp.base.Handler;
-import it.unical.mat.embasp.base.InputProgram;
-import it.unical.mat.embasp.base.Output;
-import it.unical.mat.embasp.languages.IllegalAnnotationException;
-import it.unical.mat.embasp.languages.ObjectNotValidException;
-import it.unical.mat.embasp.languages.asp.ASPInputProgram;
-import it.unical.mat.embasp.languages.asp.ASPMapper;
-import it.unical.mat.embasp.languages.asp.AnswerSet;
-import it.unical.mat.embasp.languages.asp.AnswerSets;
-import it.unical.mat.embasp.platforms.desktop.DesktopHandler;
-import it.unical.mat.embasp.specializations.dlv.desktop.DLVDesktopService;
-import it.unical.mat.embasp.specializations.dlv2.desktop.DLV2DesktopService;
-
 public class MainClass {
 
 	public MainClass() {}
